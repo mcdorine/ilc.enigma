36b8502967ef36933e35f4780386fbeb31c48192
dm663891 <dm663891@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-13 12:09:56 +0100

    _ Ajout de tests pour la classe RotorTest sur les fonctions : * convertForward * convertBackward

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -6,9 +6,25 @@ import org.junit.Test;
 
 public class RotorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    final int modu = 26;
+    String tab = "N Z J H G R C X M Y S W B O U F A I V L P E K Q D T";
+    
+    @Test
+    public void testForward() {
+    Rotor rotor5 = Rotor.rotorFactory(tab, "Z");
+    int p=12;
+    int actual = rotor5.convertForward(p);
+    assertTrue(actual<modu);
+    assertTrue(actual>=0);
+}
+
+    @Test
+    public void testBackward() {
+    Rotor rotor5 = Rotor.rotorFactory(tab, "Z");
+    int e=12;
+    int actual = rotor5.convertBackward(e);
+    assertTrue(actual<modu);
+    assertTrue(actual>=0);
+    }
 
 }
